5b3a938af7847a39f651709706284815f8d9f993
maimux2x <m0smile10often@gmail.com>
2026-04-08 21:08:46 +0900

    Manage client_id and client_secret by environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google.client_id!, Rails.application.credentials.google.client_secret!
+  provider :google_oauth2, Rails.application.config_for(:google_oauth2).client_id!, Rails.application.config_for(:google_oauth2).client_secret!
 end
