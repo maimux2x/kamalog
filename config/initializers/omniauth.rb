e713dff678077c2ade889839ee57abadffbac35f
maimux2x <m0smile10often@gmail.com>
2026-04-14 09:05:16 +0900

    Manage seeds data

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   google_oauth2 = Rails.application.config_for(:google_oauth2)
 
+  provider :developer unless Rails.env.production?
   provider :google_oauth2, google_oauth2.client_id!, google_oauth2.client_secret!
 end
