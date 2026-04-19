279c868960c6e2fe956e5f6c06089248488bb3ac
maimux2x <m0smile10often@gmail.com>
2026-04-19 13:47:53 +0900

    Manage button display

--- app/javascript/controllers/clay_usages_form_controller.js
+++ app/javascript/controllers/clay_usages_form_controller.js
@@ -1,21 +1,60 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["template", "usages", "addedUsage"]
+  static targets = ['template', 'usages', 'item']
 
-  add(event) {
+  connect() {
+    this.#manageButtonDisplay();
+  }
+
+  add() {
     const template = this.templateTarget;
     const usages = this.usagesTarget;
 
     const clone = template.content.cloneNode(true);
 
+    const index = Date.now();
+
+    clone.querySelectorAll('input, select').forEach((el) => {
+      el.id = el.id.replace('NEW_INDEX', index);
+      el.name = el.name.replace('NEW_INDEX', index);
+    })
+
     usages.appendChild(clone);
+    this.#manageButtonDisplay();
   }
 
-  remove() {
-    const added = this.addedUsageTarget;
+  remove(event) {
     const usages = this.usagesTarget;
+    const item = event.target.closest("div[class='d-flex gap-3']");
 
-    usages.removeChild(added);
+    if (item.querySelector("input[name*='_destroy']")) {
+      item.querySelector("input[name*='_destroy']").value = 1;
+      item.classList.replace('d-flex', 'd-none');
+    } else {
+      usages.removeChild(item);
+    }
+
+    this.#manageButtonDisplay();
+  }
+
+  #manageButtonDisplay() {
+    let hideItemsCount = 0;
+
+    this.itemTargets.forEach((item) => {
+      if (item.className === 'd-none gap-3') {
+        hideItemsCount++;
+      }
+    })
+
+    if (this.itemTargets.length - hideItemsCount === 1) {
+      this.itemTargets.forEach((item) => {
+        item.querySelector('button').style.display = 'none';
+      })
+    } else {
+      this.itemTargets.forEach((item) => {
+        item.querySelector('button').style.display = '';
+      })
+    }
   }
 }
