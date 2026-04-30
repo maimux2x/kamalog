daf3acf0f23c80344f5402428c180be57746f670
maimux2x <m0smile10often@gmail.com>
2026-04-30 09:46:25 +0900

    Add system test

--- test/system/my/pieces_test.rb
+++ test/system/my/pieces_test.rb
@@ -5,59 +5,41 @@ class PiecesTest < ApplicationSystemTestCase
     sign_in_as users(:alice)
   end
 
-  test 'should create Piece (with one clay, without glaze)' do
-    visit my_in_progress_index_path
-    assert_selector 'h1', text: '製作中一覧'
-
-    click_on '制作する作品を登録'
-
-    fill_in 'タイトル', with: '花瓶'
-    choose '電動ろくろ'
-    select '白土',      from: 'Clay'
-    fill_in 'Weight',   with: '500'
-
-    click_on '登録する'
-
-    assert_text '製作中の作品を登録しました。'
-    assert_text '白土'
-    assert_text '500g'
-  end
-
-  test 'should create Piece (with one clay and one glaze)' do
+  test '作品を登録する' do
     visit my_in_progress_index_path
 
     click_on '制作する作品を登録'
 
     fill_in 'タイトル', with: 'お茶碗'
     choose '電動ろくろ'
-    select '白土',      from: 'Clay'
-    fill_in 'Weight',   with: '500'
 
-    assert_selector 'button[data-nested-form-target="removeButton"][disabled]'
+    within '[data-nested-form-target="item"]' do
+      assert_selector 'button[disabled]', text: '削除'
+
+      select '白土',      from: '土'
+      fill_in '使用量',   with: '500'
+    end
 
     click_on '釉薬を追加する'
 
-    select '白マット', from: 'Glaze'
+    select '白マット', from: '釉薬'
 
     click_on '登録する'
 
     assert_text '製作中の作品を登録しました。'
-    assert_text '白土'
-    assert_text '500g'
+    assert_text '白土 500g'
     assert_text '白マット'
   end
 
-  test 'should edit Piece' do
+  test '作品のタイトルを更新する' do
     visit my_piece_path(pieces(:cup))
-
-    assert_selector 'h1', text: 'マグカップ'
 
     click_on '編集'
 
     assert_field 'タイトル', with: 'マグカップ'
     assert_checked_field '紐作り'
-    assert_select 'Clay',    selected: '白土'
-    assert_select 'Glaze',   selected: '白マット'
+    assert_select '土',    selected: '白土'
+    assert_select '釉薬',   selected: '白マット'
 
     fill_in 'タイトル', with: '大きいマグカップ'
 
@@ -66,50 +48,43 @@ class PiecesTest < ApplicationSystemTestCase
     assert_text '製作中の作品を更新しました。'
     assert_text '大きいマグカップ'
   end
-
-  test 'should edit Piece (add clay and glaze)' do
+  test '作品に土と釉薬を追加する' do
     visit my_piece_path(pieces(:cup))
-
-    assert_selector 'h1', text: 'マグカップ'
 
     click_on '編集'
 
     click_on '土を追加する'
 
-    within '.clays [data-nested-form-target="item"]:last-child' do
-      select '黒土',    from: 'Clay'
-      fill_in 'Weight', with: '100'
+    within '.clays [data-nested-form-target="item"]:last-of-type' do
+      select '黒土',    from: '土'
+      fill_in '使用量', with: '100'
     end
 
     click_on '釉薬を追加する'
 
-    within '.glazes [data-nested-form-target="item"]:last-child' do
-      select '黒マット', from: 'Glaze'
+    within '.glazes [data-nested-form-target="item"]:last-of-type' do
+      select '黒マット', from: '釉薬'
     end
 
     click_on '更新する'
 
     assert_text '製作中の作品を更新しました。'
-    assert_text '白土'
-    assert_text '500g'
-    assert_text '黒土'
-    assert_text '100g'
+    assert_text '白土 500g'
+    assert_text '黒土 100g'
     assert_text '白マット'
     assert_text '黒マット'
   end
 
-  test 'should edit Piece (delete clay and glaze)' do
+  test '作品から土と釉薬を削除する' do
     visit my_piece_path(pieces(:base))
-
-    assert_selector 'h1', text: '花瓶'
 
     click_on '編集'
 
-    within all('.clays [data-nested-form-target="item"]').last do
+    within '.clays [data-nested-form-target="item"]:last-of-type' do
       click_on '削除'
     end
 
-    within all('.glazes [data-nested-form-target="item"]').last do
+    within '.glazes [data-nested-form-target="item"]:last-of-type' do
       click_on '削除'
     end
 
