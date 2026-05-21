bc9026636a470efbf92dc3482cd2dc2f97bc97b9
maimux2x <m0smile10often@gmail.com>
2026-05-21 14:56:59 +0900

    Improve layout

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :log
 
-  has_one_attached :file do |attachable|
-    attachable.variant :small, resize_to_limit: [100, 100]
-  end
+  has_one_attached :file
 end
