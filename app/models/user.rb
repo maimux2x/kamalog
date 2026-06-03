199e7446cd9e29dde534cc763941a0b813a057b4
maimux2x <m0smile10often@gmail.com>
2026-06-03 13:41:21 +0900

    Refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
+  has_one_attached :avatar do |attachable|
+    attachable.variant :small, resize_to_limit: [200, 200]
+  end
 
   has_many :pieces, dependent: :destroy
 end
