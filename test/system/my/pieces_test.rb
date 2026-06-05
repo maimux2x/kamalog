0e81be3cd5652cb90deb06ff6c036d3fa55187fc
maimux2x <m0smile10often@gmail.com>
2026-06-05 16:40:35 +0900

    Fix test

--- test/system/my/pieces_test.rb
+++ test/system/my/pieces_test.rb
@@ -3,23 +3,26 @@ require 'application_system_test_case'
 class My::PiecesTest < ApplicationSystemTestCase
   setup do
     sign_in_as users(:alice)
+
+    @studio = users(:alice).studios.first
+    @piece  = pieces(:cup)
   end
 
   test '作品一覧を表示する' do
-    visit studio_my_pieces_path(studios(:wonderland))
+    visit studio_my_pieces_path(@studio)
 
     assert_link 'マグカップ'
     assert_link '花瓶'
   end
 
   test '詳細を表示する' do
-    visit studio_my_piece_path(studios(:wonderland), pieces(:cup))
+    visit studio_my_piece_path(@studio, @piece)
 
     assert_text 'マグカップ'
   end
 
   test '作品を削除する' do
-    visit studio_my_piece_path(studios(:wonderland), pieces(:cup))
+    visit studio_my_piece_path(@studio, @piece)
 
     click_on '削除'
 
