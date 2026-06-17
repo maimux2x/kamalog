a1d85cbbd3b5c5c893c27bba73430384d4a541b9
maimux2x <m0smile10often@gmail.com>
2026-06-17 11:30:26 +0900

    Fix test

--- test/system/clays/bulks_test.rb
+++ test/system/clays/bulks_test.rb
@@ -18,9 +18,41 @@ class Clays::BulksTest < ApplicationSystemTestCase
 
     click_on '一括登録'
 
-    assert_text '土を一括登録しました。'
+    assert_text '3件の土を一括登録しました。'
     assert_text '土A'
     assert_text '土B'
     assert_text '土C'
+  end
+
+  test 'すでに保存済みの土はスキップされること' do
+    visit new_studio_clays_bulk_path(@studio)
+
+    fill_in '名前', with: <<~TEXT
+      白土
+      黒土
+      土A
+    TEXT
+
+    click_on '一括登録'
+
+    assert_text '1件の土を一括登録しました。'
+    assert_text '土A'
+  end
+
+  test '空や空白だけの行は保存されないこと' do
+    visit new_studio_clays_bulk_path(@studio)
+
+    # rubocop:disable Layout/TrailingWhitespace
+    fill_in '名前', with: <<~TEXT
+         
+
+      土A
+    TEXT
+    # rubocop:disable Layout/TrailingWhitespace
+
+    click_on '一括登録'
+
+    assert_text '1件の土を一括登録しました。'
+    assert_text '土A'
   end
 end
