1ab36fff1fd16451ec4150763dff14401b0a881d
maimux2x <m0smile10often@gmail.com>
2026-06-17 11:29:55 +0900

    Improve success message

--- app/controllers/clays/bulks_controller.rb
+++ app/controllers/clays/bulks_controller.rb
@@ -4,10 +4,10 @@ class Clays::BulksController < ApplicationController
   def new; end
 
   def create
-    current_studio.clays.insert_all params[:names].lines(chomp: true).compact_blank.map {|name|
+    result = current_studio.clays.insert_all(params[:names].lines(chomp: true).compact_blank.map {|name|
       {name:}
-    }, unique_by: :name
+    }, unique_by: :name)
 
-    redirect_to studio_clays_path(current_studio), status: :see_other, notice: '土を一括登録しました。'
+    redirect_to studio_clays_path(current_studio), status: :see_other, notice: "#{result.rows.length}件の土を一括登録しました。"
   end
 end
