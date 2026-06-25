e6ea0e2437054ead7d1a69a0a2eb037bbb18731a
maimux2x <m0smile10often@gmail.com>
2026-06-25 14:33:55 +0900

    Tooltip

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -1,11 +1,33 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class TooltipController extends Controller {
+  static values = {
+    title: String
+  };
+
   connect() {
+    this.tooltip = new bootstrap.Tooltip(this.element, {
+      trigger: 'manual',
+      title: this.titleValue
+    });
+
+    this.element.addEventListener('shown.bs.tooltip', () => {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+
+      this.timer = setTimeout(() => {
+        this.tooltip.hide();
+      }, 1000);
+    });
   }
 
-  finish() {
-    new bootstrap.Tooltip(this.element);
+  disconnect() {
+    this.tooltip.dispose();
+  }
+
+  show() {
+    this.tooltip.show();
   }
 }
 
