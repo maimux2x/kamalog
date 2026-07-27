cb492265fa90971eb2b8618bc32f765447e9340b
maimux2x <m0smile10often@gmail.com>
2026-07-27 13:43:49 +0900

    Allow changing column name for sort order

--- app/models/concerns/position.rb
+++ app/models/concerns/position.rb
@@ -2,10 +2,12 @@ module Position
   extend ActiveSupport::Concern
 
   class_methods do
-    def positioned(on:)
+    def positioned(on:, column: :position)
+      quoted_column = ActiveRecord::Base.connection.quote_column_name(column)
+
       define_method :move_to do |position|
         rel = instance_exec(&on)
-        arr = rel.order(:position).to_a
+        arr = rel.order(column).to_a
 
         arr.delete self
         arr.insert position - 1, self
@@ -13,15 +15,15 @@ module Position
         cond = arr.size.times.map { 'WHEN ? THEN ?' }.join(' ')
         args = arr.flat_map.with_index(1) {|target, i| [target.id, i] }
 
-        rel.update_all 'position = -position'
-        rel.update_all ActiveRecord::Base.sanitize_sql_array(["position = CASE id #{cond} END", *args])
+        rel.update_all "#{quoted_column} = -#{quoted_column}"
+        rel.update_all ActiveRecord::Base.sanitize_sql_array(["#{quoted_column} = CASE id #{cond} END", *args])
       end
 
       after_destroy do
         rel = instance_exec(&on)
 
-        rel.where('position > ?', position).update_all 'position = -position'
-        rel.where('position < ?', 0).update_all 'position = -position - 1'
+        rel.where("#{quoted_column} > ?", self[column]).update_all "#{quoted_column} = -#{quoted_column}"
+        rel.where("#{quoted_column} < ?", 0).update_all "#{quoted_column} = -#{quoted_column} - 1"
       end
     end
   end
