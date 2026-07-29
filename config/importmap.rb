4942b67914d906b8c8894dd17bbbbdaad4315f81
maimux2x <m0smile10often@gmail.com>
2026-07-29 09:30:34 +0900

    Rubocop

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,11 +2,11 @@
 
 pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin '@hotwired/stimulus', to: '@hotwired--stimulus.js' # @3.2.2
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin '@popperjs/core', to: 'popper.js', preload: true
-pin "@stimulus-components/sortable", to: "@stimulus-components--sortable.js" # @5.0.3
-pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.13
-pin "sortablejs" # @1.15.7
+pin '@stimulus-components/sortable', to: '@stimulus-components--sortable.js' # @5.0.3
+pin '@rails/request.js', to: '@rails--request.js.js' # @0.0.13
+pin 'sortablejs' # @1.15.7
