e9c5536c03ca1e852c94df5fe6450358fc7a560d
maimux2x <m0smile10often@gmail.com>
2026-07-30 10:46:39 +0900

    Fix test

--- test/system/glazes_js_test.rb
+++ test/system/glazes_js_test.rb
@@ -13,25 +13,21 @@ class GlazesJSTest < ApplicationSystemTestCase
   test '釉薬の表示順の並び替えができること' do
     visit studio_glazes_path(@studio)
 
-    within 'ul.list-group li:nth-child(3)' do
-      select 2, from: 'glaze[position]'
-    end
+    source = find('ul.list-group li:nth-child(3) .handle')
+    target = find('ul.list-group li:nth-child(2)')
 
-    assert_text '釉薬を更新しました。'
+    source.drag_to target
 
     within 'ul.list-group li:nth-child(1)' do
       assert_text '白マット'
-      assert_select 'glaze[position]', selected: '1'
     end
 
     within 'ul.list-group li:nth-child(2)' do
       assert_text 'パールラスター'
-      assert_select 'glaze[position]', selected: '2'
     end
 
     within 'ul.list-group li:nth-child(3)' do
       assert_text '黒マット'
-      assert_select 'glaze[position]', selected: '3'
     end
   end
 end
