1937de3bd3708bf91ee24601ff1cc20c80123917
maimux2x <m0smile10often@gmail.com>
2026-08-26 11:59:34 +0900

    Use first position for image display in list view

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,7 +2,7 @@ class PiecesController < ApplicationController
   include CurrentMembership
 
   def index
-    @pagy, @pieces = pagy(current_studio.pieces.complete.order({completed_at: :desc}, :id))
+    @pagy, @pieces = pagy(current_studio.pieces.complete.order({completed_at: :desc}, :id, 'photos.position'))
   end
 
   def show
