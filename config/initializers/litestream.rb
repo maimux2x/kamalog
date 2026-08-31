ad30b4cbbe998d04bac3d4117916f83146f01c86
maimux2x <m0smile10often@gmail.com>
2026-08-31 17:45:43 +0900

    Use litestream

---
+++ config/initializers/litestream.rb
@@ -0,0 +1,9 @@
+Rails.application.configure do
+  litestream = Rails.application.credentials.litestream
+
+  config.litestream.access_key_id     = litestream.access_key_id
+  config.litestream.secret_access_key = litestream.secret_access_key
+
+  config.litestream.username = litestream.username
+  config.litestream.password = litestream.password
+end
