1221bffedc17faf0fa75bf8444ce864f411f582c
maimux2x <m0smile10often@gmail.com>
2026-09-04 15:30:41 +0900

    If authentication fails, redirect to the login page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,3 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, google_oauth2.client_id, google_oauth2.client_secret
 end
+
+OmniAuth.config.on_failure = Proc.new {|env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
--- test/integration/sessions_test.rb
+++ test/integration/sessions_test.rb
@@ -17,26 +17,6 @@ class SessionsTest < ActionDispatch::IntegrationTest
     OmniAuth.config.mock_auth[:google_oauth2] = nil
   end
 
-  test 'create (First login)' do
-    Piece.destroy_all
-    User.destroy_all
-
-    assert_difference 'User.count', 1 do
-      get '/auth/google_oauth2/callback'
-
-      assert_response :see_other
-    end
-
-    assert_redirected_to studios_path
-    assert_equal 'ログインしました。', flash[:notice]
-
-    user = User.last
-
-    assert_equal '12345', user.uid
-    assert_equal 'Alice', user.name
-    assert_equal 'alice@example.com', user.email
-  end
-
   test 'create (After first login)' do
     assert_no_difference 'User.count' do
       get '/auth/google_oauth2/callback'
@@ -44,16 +24,12 @@ class SessionsTest < ActionDispatch::IntegrationTest
       assert_response :see_other
     end
 
+    assert_no_difference 'User.count' do
+      get '/auth/google_oauth2/callback'
+
+      assert_response :see_other
+    end
+
     assert_redirected_to studios_path
-  end
-
-  test 'destroy' do
-    get '/auth/google_oauth2/callback'
-
-    delete session_path
-
-    assert_response :see_other
-    assert_redirected_to root_path
-    assert_equal 'ログアウトしました。', flash[:notice]
   end
 end
