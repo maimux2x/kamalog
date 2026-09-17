9fe401b9737db2381aa750942fd061e6158c3950
maimux2x <m0smile10often@gmail.com>
2026-09-17 10:38:02 +0900

    Fix test

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,6 +1,6 @@
 class WelcomesController < ApplicationController
   def show
-    if studio = current_user.last_seen_studio || current_user.studios.first
+    if studio = current_user.last_seen_studio || current_user.studios.order('memberships.id').first
       redirect_to studio_my_pieces_path(studio), status: :see_other
     end
   end
--- test/system/login_test.rb
+++ test/system/login_test.rb
@@ -1,7 +1,7 @@
 require 'application_system_test_case'
 
 class LoginTest < ApplicationSystemTestCase
-  test '教室に所属している場合ログインすると作品一覧が表示される' do
+  test '複数の教室に所属していて、最後に見ていた教室がない場合ログインすると最初に参加した教室の作品一覧が表示される' do
     visit root_path
 
     mock_auth users(:alice) do
@@ -9,7 +9,20 @@ class LoginTest < ApplicationSystemTestCase
     end
 
     assert_text 'ログインしました。'
-    assert_link '作品を登録'
+    assert_current_path studio_my_pieces_path(studios(:mirrorland))
+  end
+
+  test '複数の教室に所属していて、最後に見ていた教室がある場合、その教室の作品一覧が表示される' do
+    users(:alice).update! last_seen_studio: studios(:wonderland)
+
+    visit root_path
+
+    mock_auth users(:alice) do
+      click_on 'Google で続ける'
+    end
+
+    assert_text 'ログインしました。'
+    assert_current_path studio_my_pieces_path(studios(:wonderland))
   end
 
   test '教室に所属していない場合メッセージが表示される' do
